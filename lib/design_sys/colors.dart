424566ff0cb9dceacf137854c8d79661229fd152
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-09-22 21:32:39 -0300

    colors init

---
+++ lib/design_sys/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+
+class AppColors {
+  AppColors._();
+
+  static const Color black = Color(0xff10111d);
+  static const Color white = Color(0xfffaf6ed);
+  static const Color income = Color(0xff0f9015);
+  static const Color expense = Color(0xffe53529);
+  static const Color splashDark = Color(0x44000000);
+  static const Color splashLight = Color(0x44ffffff);
+  static const Color primary = Color(0xff000000);
+}
