e09d7203d5f8c6f1c59bb70be4a766303bb54e08
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-09-30 00:52:43 -0300

    feat: edit entry dialog

--- lib/home/new_entry_dialog.dart
+++ lib/home/new_entry_dialog.dart
@@ -1,97 +1,119 @@
 import 'package:financial_app/design_sys/colors.dart';
 import 'package:flutter/material.dart';
 
-import '../widgets/app_date_picker.dart';
-import '../widgets/app_dropdown_button_form_field.dart';
-import '../widgets/app_toggle_buttons.dart';
+import 'widgets/app_date_picker.dart';
+import 'widgets/app_dropdown_button_form_field.dart';
+import 'widgets/app_toggle_buttons.dart';
 
 Future<void> newEntryDialog(BuildContext context, String type) {
-  final List<bool> select = <bool>[true, false, false];
-  final List<String> expenseCategories = <String>[
-    'Alimentação',
-    'Combustível',
-    'Saúde',
-    'Água',
-    'Energia',
-  ];
-  final List<String> incomeCategories = <String>[
-    'Salário',
-    'Presente',
-  ];
-  final categories = type == 'income' ? incomeCategories : expenseCategories;
-  final color = type == 'income' ? AppColors.income : AppColors.expense;
-  final title = type == 'income' ? 'Nova receita' : 'Nova despesa';
-
   return showDialog(
       context: context,
       builder: (BuildContext context) {
-        return NewEntryDialog(
-          categories: categories,
-          color: color,
-          select: select,
-          title: title,
-        );
+        return NewEntryDialog();
       });
 }
 
-class NewEntryDialog extends StatelessWidget {
-  const NewEntryDialog({
-    Key? key,
-    required List<String> categories,
-    required Color color,
-    required List<bool> select,
-    required String title,
-  })  : _categories = categories,
-        _color = color,
-        _select = select,
-        _title = title,
-        super(key: key);
+final List<String> expenseCategories = <String>[
+  'Alimentação',
+  'Combustível',
+  'Saúde',
+  'Água',
+  'Energia',
+];
+final List<String> incomeCategories = <String>[
+  'Salário',
+  'Presente',
+];
 
-  final List<String> _categories;
-  final Color _color;
-  final List<bool> _select;
-  final String _title;
+class NewEntryDialog extends StatefulWidget {
+  NewEntryDialog({Key? key}) : super(key: key);
+
+  final List<bool> _selectType = <bool>[true, false];
+  final List<bool> _select = <bool>[true, false, false];
+
+  @override
+  State<NewEntryDialog> createState() => _NewEntryDialogState();
+}
+
+class _NewEntryDialogState extends State<NewEntryDialog> {
+  List<String> _categories = expenseCategories;
+  Color _color = AppColors.expense;
 
   @override
   Widget build(BuildContext context) {
+    var topBar = ToggleButtons(
+      constraints: const BoxConstraints.expand(width: 146),
+      selectedColor: _color,
+      fillColor: AppColors.white,
+      renderBorder: false,
+      textStyle: const TextStyle(
+        fontWeight: FontWeight.w500,
+        fontSize: 16,
+      ),
+      onPressed: (index) {
+        setState(() {
+          for (int i = 0; i < widget._selectType.length; i++) {
+            widget._selectType[i] = i == index;
+            _categories = index == 0 ? expenseCategories : incomeCategories;
+            _color = index == 0 ? AppColors.expense : AppColors.income;
+          }
+        });
+      },
+      borderRadius: const BorderRadius.vertical(top: Radius.circular(4)),
+      isSelected: widget._selectType,
+      children: const [
+        Padding(
+          padding: EdgeInsets.all(16),
+          child: Text('Despesa'),
+        ),
+        Padding(
+          padding: EdgeInsets.all(16),
+          child: Text('Receita'),
+        ),
+      ],
+    );
+
     return AlertDialog(
       backgroundColor: AppColors.white,
-      titleTextStyle:
-          TextStyle(color: _color, fontWeight: FontWeight.w500, fontSize: 16),
-      title: Text(_title),
-      content: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          TextFormField(
-            autofocus: true,
-            decoration: InputDecoration(
-              labelStyle: TextStyle(color: _color.withOpacity(0.6)),
-              labelText: 'Valor',
-              prefixText: 'R\$ ',
+      titlePadding: const EdgeInsets.all(0),
+      title: topBar,
+      content: Form(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            TextFormField(
+              autofocus: true,
+              decoration: InputDecoration(
+                floatingLabelStyle: TextStyle(color: _color),
+                labelStyle: TextStyle(color: _color.withOpacity(0.6)),
+                labelText: 'Valor',
+                prefixText: 'R\$ ',
+              ),
             ),
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          TextFormField(
-            decoration: InputDecoration(
-              labelStyle: TextStyle(color: _color.withOpacity(0.6)),
-              labelText: 'Descrição',
+            const SizedBox(
+              height: 8,
             ),
-          ),
-          const SizedBox(
-            height: 8,
-          ),
-          AppDropdownButtonFormField(categories: _categories),
-          const SizedBox(
-            height: 16,
-          ),
-          AppToggleButtons(select: _select, color: _color),
-          const SizedBox(
-            height: 8,
-          ),
-          AppDatePicker(color: _color),
-        ],
+            TextFormField(
+              decoration: InputDecoration(
+                floatingLabelStyle: TextStyle(color: _color),
+                labelStyle: TextStyle(color: _color.withOpacity(0.6)),
+                labelText: 'Descrição',
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            AppDropdownButtonFormField(categories: _categories),
+            const SizedBox(
+              height: 16,
+            ),
+            AppToggleButtons(select: widget._select, color: _color),
+            const SizedBox(
+              height: 8,
+            ),
+            AppDatePicker(color: _color),
+          ],
+        ),
       ),
       actions: [
         TextButton(
@@ -104,3 +126,5 @@ class NewEntryDialog extends StatelessWidget {
     );
   }
 }
+
+//textInputAction
