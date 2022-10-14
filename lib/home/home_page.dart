16528c46eb44ee8992d84b1c4fac32c83d8f0e69
rudarabello <ruda.rabello.silva@gmail.com>
2022-10-13 21:37:02 -0300

    fix: delete Text on home page

--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -16,7 +16,6 @@ class _HomePageState extends State<HomePage> {
   final List<Widget> _pages = const [
     HomeContentPage(),
     Center(child: Text("Page Extrato")),
-    Text(''),
     Center(child: Text('Page Estatítica')),
     Center(child: Text('Page mais')),
   ];
