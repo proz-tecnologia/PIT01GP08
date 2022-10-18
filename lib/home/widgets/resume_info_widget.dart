2ae954a374fd09e63d87e9f385efec14919abc61
rudarabello <ruda.rabello.silva@gmail.com>
2022-10-18 16:19:04 -0300

    feat: add initial structure on resume info

--- lib/home/widgets/resume_info_widget.dart
+++ lib/home/widgets/resume_info_widget.dart
@@ -1,8 +1,6 @@
 import 'package:financial_app/design_sys/colors.dart';
 import 'package:flutter/material.dart';
 
-import 'item_to_expansion_panel.dart';
-
 class ResumeInfoWidget extends StatefulWidget {
   const ResumeInfoWidget({
     Key? key,
@@ -15,40 +13,142 @@ class ResumeInfoWidget extends StatefulWidget {
 class _ResumeInfoWidgetState extends State<ResumeInfoWidget> {
   @override
   Widget build(BuildContext context) {
-    return Card(
+    return Container(
       margin: const EdgeInsets.only(bottom: 16),
-      borderOnForeground: false,
-      elevation: 2,
+      height: 250,
+      width: MediaQuery.of(context).size.width,
       color: AppColors.primary,
-      child: InkWell(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              height: 250,
-              width: MediaQuery.of(context).size.width,
-              child:
-                  TextButton(onPressed: () {}, child: const Text("Dezembro")),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        children: [
+          const SizedBox(
+            height: 8,
+          ),
+          SizedBox(
+            width: MediaQuery.of(context).size.width,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                const SizedBox(
+                  width: 80,
+                ),
+                IconButton(
+                    color: AppColors.white,
+                    onPressed: () {},
+                    icon: const Icon(Icons.arrow_back_ios)),
+                const Text(
+                  'Dezembro',
+                  style: TextStyle(
+                    color: AppColors.white,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
+                IconButton(
+                    color: AppColors.white,
+                    onPressed: () {},
+                    icon: const Icon(Icons.arrow_forward_ios)),
+                const SizedBox(
+                  width: 80,
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+          SizedBox(
+            height: 45,
+            width: MediaQuery.of(context).size.width,
+          ),
+          const Text(
+            'Saldo',
+            style: TextStyle(
+              color: AppColors.white,
+              fontWeight: FontWeight.w400,
+            ),
+          ),
+          const Text(
+            'R\$ 2600,00',
+            style: TextStyle(
+              color: AppColors.white,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          SizedBox(
+            height: 45,
+            width: MediaQuery.of(context).size.width,
+          ),
+          Container(
+            color: AppColors.primary,
+            child: Row(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.arrow_downward,
+                        size: 30,
+                        color: AppColors.white,
+                      ),
+                      Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: const [
+                          Text(
+                            'Receitas',
+                            style: TextStyle(
+                              color: AppColors.white,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            'RS 1000,00',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const SizedBox(
+                  width: 60,
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    children: [
+                      const Icon(
+                        Icons.arrow_downward,
+                        size: 30,
+                        color: AppColors.white,
+                      ),
+                      Column(
+                        mainAxisSize: MainAxisSize.max,
+                        children: const [
+                          Text(
+                            'Receitas',
+                            style: TextStyle(
+                              color: AppColors.white,
+                              fontWeight: FontWeight.w400,
+                            ),
+                          ),
+                          Text(
+                            'RS 1000,00',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
 }
-//   @override
-//   Widget build(BuildContext context) {
-//     return Card(
-//       margin: const EdgeInsets.all(16),
-//       child: InkWell(
-//         onTap: () {
-//           //TODO: navigation
-//         },
-//         child: SizedBox(
-//           height: 195,
-//           child: 
-//         ),
-//       ),
-//     );
-//   }
-// }
