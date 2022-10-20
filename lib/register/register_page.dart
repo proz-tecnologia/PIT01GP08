64a1a005a8f0cedb07152ffb1a60dfb6ba8ab9ff
jacksonlira88 <jacksonlira.rs@gmail.com>
2022-10-20 20:20:18 -0300

    feat: screem for register page.

---
+++ lib/register/register_page.dart
@@ -0,0 +1,33 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  const RegisterPage({super.key});
+
+  @override
+  State<RegisterPage> createState() => RegisterPageState();
+}
+
+class RegisterPageState extends State<RegisterPage> {
+  final TextInputAction _textInputActionNext = TextInputAction.next;
+
+  @override
+  Widget build(BuildContext context) {
+    final sizeHeight = MediaQuery.of(context).size.height;
+    final double sizeSpaceTitleTop = sizeHeight * 0.22;
+    final double sizeSpaceItem = sizeHeight * 0.01;
+    final double sizeButton = sizeHeight * 0.072;
+    final double sizeSpaceItemButton = sizeHeight * 0.055;
+    final double sizeSpaceItemEnd = sizeHeight * 0.08;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(mainAxisSize: MainAxisSize.min, children: []),
+        ),
+      ),
+    );
+  }
+}
