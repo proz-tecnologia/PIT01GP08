971981ad7455d67ac5880aca8e5f4d691cf620df
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-10-22 00:42:28 -0300

    fix: checkbox update

--- lib/shared/new_entry/components/fulfilled_check.dart
+++ lib/shared/new_entry/components/fulfilled_check.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AppFulfilledCheck extends StatefulWidget {
-  const AppFulfilledCheck({
+  AppFulfilledCheck({
     Key? key,
     required Color color,
     required String fulfilledLabel,
@@ -13,25 +13,23 @@ class AppFulfilledCheck extends StatefulWidget {
 
   final Color _color;
   final String _fulfilledLabel;
-  final bool _initialFulfilled;
+  bool _initialFulfilled;
 
   @override
   State<AppFulfilledCheck> createState() => _AppFulfilledCheckState();
 }
 
 class _AppFulfilledCheckState extends State<AppFulfilledCheck> {
-  bool isFulfilled = true;
   @override
   Widget build(BuildContext context) {
-    isFulfilled = widget._initialFulfilled;
     return Row(
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Checkbox(
-          value: isFulfilled,
+          value: widget._initialFulfilled,
           onChanged: (value) {
             setState(() {
-              isFulfilled = value!;
+              widget._initialFulfilled = value!;
             });
           },
           fillColor: MaterialStateProperty.all(widget._color),
