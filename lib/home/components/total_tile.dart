027c90fdf94a1fcb0ec6a2ecbbf79efc853a81bf
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-10-27 23:09:58 -0300

    feat: resume visibility

---
+++ lib/home/components/total_tile.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+
+class TotalTile extends StatelessWidget {
+  const TotalTile({
+    Key? key,
+    this.icon,
+    required this.label,
+    required this.value,
+    required this.visible,
+  }) : super(key: key);
+
+  final IconData? icon;
+  final String label;
+  final String value;
+  final bool visible;
+
+  @override
+  Widget build(BuildContext context) {
+    final hidden = Container(
+      color: Theme.of(context).colorScheme.onPrimary,
+      width: 72.0,
+      height: 11.0,
+      margin: const EdgeInsets.symmetric(vertical: 4.0),
+    );
+    final leading = icon == null
+        ? Container()
+        : Padding(
+            padding: const EdgeInsets.only(right: 8.0),
+            child: Icon(
+              icon,
+              size: 32,
+              color: Theme.of(context).colorScheme.onPrimary,
+            ),
+          );
+    return Row(
+      children: [
+        leading,
+        Column(
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              label,
+              style: TextStyle(
+                color: Theme.of(context).colorScheme.onPrimary,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            visible
+                ? Text(
+                    value,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).colorScheme.onPrimary,
+                    ),
+                  )
+                : hidden,
+          ],
+        ),
+      ],
+    );
+  }
+}
