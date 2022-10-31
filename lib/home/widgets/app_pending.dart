bd94e9dcccc927975299c49e7608de7efd387a52
edilson@Acer-E5-473 <ed.vilarinho@gmail.com>
2022-10-31 17:18:57 -0300

    refactor: 1a revisão - inclusão /home/components/pending_card.dart

--- lib/home/widgets/app_pending.dart
+++ lib/home/widgets/app_pending.dart
@@ -2,6 +2,8 @@ import 'package:financial_app/design_sys/colors.dart';
 import 'package:financial_app/design_sys/sizes.dart';
 import 'package:flutter/material.dart';
 
+import '../components/pending_card.dart';
+
 class AppPending extends StatelessWidget {
   const AppPending({
     Key? key,
@@ -12,57 +14,23 @@ class AppPending extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.all(Sizes.smallSpace),
       child: Row(
-        children: [
+        children: const [
           Expanded(
             flex: 1,
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(Sizes.mediumSpace),
-                child: Column(
-                  children: const [
-                    Icon(
-                      Icons.download_rounded,
-                      size: Sizes.largeIconSize,
-                      color: AppColors.expense,
-                    ),
-                    Text('Pagar'),
-                    Text(
-                      'RS 1000,00',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.expense,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+            child: PendingCard(
+              icon: Icons.download_rounded,
+              color: AppColors.expense,
+              label: 'A pagar',
+              value: 'R\$ 1000,00',
             ),
           ),
           Expanded(
             flex: 1,
-            child: Card(
-              child: Padding(
-                padding: const EdgeInsets.all(Sizes.mediumSpace),
-                child: Column(
-                  children: const [
-                    Icon(
-                      Icons.publish_rounded,
-                      size: Sizes.largeIconSize,
-                      color: AppColors.income,
-                    ),
-                    Text('Receber'),
-                    Text(
-                      'RS 1000,00',
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: AppColors.income,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
+            child: PendingCard(
+              color: AppColors.income,
+              icon: Icons.publish_rounded,
+              label: 'A receber',
+              value: 'R\$ 1000,00',
             ),
           ),
         ],
