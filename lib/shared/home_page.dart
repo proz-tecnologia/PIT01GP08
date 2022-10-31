77dc4a818f5e27dbcafc9564f051bf31f31f2648
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-10-31 09:56:18 -0300

    remove files which don't belong

--- lib/shared/home_page.dart
+++ lib/shared/home_page.dart
@@ -27,7 +27,7 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () => Navigator.of(context).pushNamed('newEntry'),
+        onPressed: () {},
         tooltip: 'Despesa',
         child: const Icon(Icons.add),
       ),
--- lib/shared/new_entry/components/components.dart
+++
@@ -1,6 +0,0 @@
-export 'currency_field.dart';
-export 'date_picker_field.dart';
-export 'dropdown_button_field.dart';
-export 'fulfilled_check.dart';
-export 'toggle_buttons.dart';
-export 'top_bar_item.dart';
