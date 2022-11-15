955cc25bed7ed777f16118582c76769a292e91e9
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-11-15 12:49:34 -0300

    feat: add total

--- lib/controllers/statistics_controller.dart
+++ lib/controllers/statistics_controller.dart
@@ -4,14 +4,14 @@ import '../models/section.dart';
 
 class StatisticsController {
   final _sections = <Section>[];
+  late double _total;
 
   StatisticsController() {
     _getSections();
   }
 
-  List<Section> get sections {
-    return _sections;
-  }
+  List<Section> get sections => _sections;
+  double get total => _total;
 
   void _getSections() {
     _sections.addAll([
@@ -20,10 +20,11 @@ class StatisticsController {
       Section(3, color: Colors.purple),
       Section(2, color: Colors.green),
     ]);
+    _total = _getTotal();
     _setPercents();
   }
 
-  double getTotal() {
+  double _getTotal() {
     double total = 0;
     for (var s in _sections) {
       total += s.value;
@@ -32,9 +33,8 @@ class StatisticsController {
   }
 
   void _setPercents() {
-    double total = getTotal();
     for (var s in _sections) {
-      s.percent = s.value / total * 100;
+      s.percent = s.value / _total * 100;
     }
   }
 }
--- lib/views/statistics/statistics_page.dart
+++ lib/views/statistics/statistics_page.dart
@@ -21,41 +21,52 @@ class _StatisticsPageState extends State<StatisticsPage> {
     final screenMinSize = min(
         MediaQuery.of(context).size.width, MediaQuery.of(context).size.height);
     return Center(
-      child: PieChart(
-        PieChartData(
-          pieTouchData: PieTouchData(
-            touchCallback: (FlTouchEvent event, pieTouchResponse) {
-              setState(() {
-                if (!event.isInterestedForInteractions ||
-                    pieTouchResponse == null ||
-                    pieTouchResponse.touchedSection == null) {
-                  touchedIndex = -1;
-                  return;
-                }
-                touchedIndex =
-                    pieTouchResponse.touchedSection!.touchedSectionIndex;
-              });
-            },
+      child: Stack(
+        alignment: AlignmentDirectional.center,
+        children: [
+          Text(
+            'Total:\nR\$${controller.total.toStringAsFixed(2).replaceFirst('.', ',')}',
+            style: Theme.of(context).textTheme.bodyLarge,
+            textAlign: TextAlign.center,
           ),
-          sectionsSpace: 0,
-          centerSpaceRadius: screenMinSize * 0.2,
-          sections: List.generate(
-            controller.sections.length,
-            (i) {
-              final isTouched = i == touchedIndex;
-              return PieChartSectionData(
-                color: controller.sections[i].color,
-                value: controller.sections[i].percent,
-                title: '${controller.sections[i].percent.toInt()}%',
-                radius: isTouched ? screenMinSize * 0.23 : screenMinSize * 0.2,
-                titleStyle: const TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: AppColors.white,
-                ),
-              );
-            },
+          PieChart(
+            PieChartData(
+              pieTouchData: PieTouchData(
+                touchCallback: (FlTouchEvent event, pieTouchResponse) {
+                  setState(() {
+                    if (!event.isInterestedForInteractions ||
+                        pieTouchResponse == null ||
+                        pieTouchResponse.touchedSection == null) {
+                      touchedIndex = -1;
+                      return;
+                    }
+                    touchedIndex =
+                        pieTouchResponse.touchedSection!.touchedSectionIndex;
+                  });
+                },
+              ),
+              sectionsSpace: 0,
+              centerSpaceRadius: screenMinSize * 0.2,
+              sections: List.generate(
+                controller.sections.length,
+                (i) {
+                  final isTouched = i == touchedIndex;
+                  return PieChartSectionData(
+                    color: controller.sections[i].color,
+                    value: controller.sections[i].percent,
+                    title: '${controller.sections[i].percent.toInt()}%',
+                    radius:
+                        isTouched ? screenMinSize * 0.23 : screenMinSize * 0.2,
+                    titleStyle: const TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: AppColors.white,
+                    ),
+                  );
+                },
+              ),
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
