7c54bb7b94da1966365481cfd654f2b6870a0691
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-11-26 00:39:41 -0300

    feat: add app bar

--- lib/features/statement/statement_page.dart
+++ lib/features/statement/statement_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../home/components/month_changer.dart';
 import 'statement_controller.dart';
 import 'statement_states.dart';
 import 'widgets/topbar_button.dart';
@@ -15,38 +16,58 @@ class StatementPage extends StatefulWidget {
 class _StatementPageState extends State<StatementPage> {
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<StatementController, StatementState>(
-      builder: (context, currentState) {
-        if (currentState is LoadingStatementState) {
-          return const Center(child: CircularProgressIndicator());
-        }
-        if (currentState is ErrorStatementState) {
-          return const Center(child: Text('Erro'));
-        }
-        final list = context.read<StatementController>().list;
-        return SafeArea(
-          child: Column(
-            children: [
-              Row(
-                children: [
-                  TopBarToggle.expense(),
-                  TopBarToggle.income(),
-                ],
-              ),
-              Expanded(
-                child: ListView.builder(
-                    itemCount: list.length,
-                    itemBuilder: (context, index) => Card(
-                          child: ListTile(
-                            title: Text(list[index].valueString),
-                            subtitle: Text(list[index].description),
-                          ),
-                        )),
-              ),
+    return Column(
+      children: [
+        AppBar(
+          centerTitle: true,
+          titleTextStyle: Theme.of(context).textTheme.bodyLarge,
+          title: Row(
+            mainAxisSize: MainAxisSize.min,
+            children: const [
+              MonthChanger('Dezembro'),
             ],
           ),
-        );
-      },
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(Icons.text_snippet_rounded),
+            ),
+          ],
+        ),
+        Expanded(
+          child: BlocBuilder<StatementController, StatementState>(
+            builder: (context, currentState) {
+              if (currentState is LoadingStatementState) {
+                return const Center(child: CircularProgressIndicator());
+              }
+              if (currentState is ErrorStatementState) {
+                return const Center(child: Text('Erro'));
+              }
+              final list = context.read<StatementController>().list;
+              return Column(
+                children: [
+                  Row(
+                    children: [
+                      TopBarToggle.expense(),
+                      TopBarToggle.income(),
+                    ],
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: list.length,
+                        itemBuilder: (context, index) => Card(
+                              child: ListTile(
+                                title: Text(list[index].valueString),
+                                subtitle: Text(list[index].description),
+                              ),
+                            )),
+                  ),
+                ],
+              );
+            },
+          ),
+        ),
+      ],
     );
   }
 }
