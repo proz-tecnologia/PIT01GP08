81925784702e0e12c17c670297fb11cc40ba455f
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-11-26 16:15:51 -0300

    refactor: componentize transaction tile

--- lib/features/statement/statement_page.dart
+++ lib/features/statement/statement_page.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../design_sys/colors.dart';
 import '../../design_sys/sizes.dart';
 import '../home/components/month_changer.dart';
 import 'statement_controller.dart';
 import 'statement_states.dart';
 import 'widgets/topbar_button.dart';
+import 'widgets/transaction_tile.dart';
 
 class StatementPage extends StatefulWidget {
   const StatementPage({super.key});
@@ -61,37 +61,7 @@ class _StatementPageState extends State<StatementPage> {
                       child: ListView.builder(
                         itemCount: list.length,
                         itemBuilder: (context, index) => Card(
-                          child: ListTile(
-                            title: Text(
-                              list[index].valueString,
-                              style: TextStyle(
-                                color: list[index].type == 'expense'
-                                    ? AppColors.expense
-                                    : AppColors.income,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            subtitle: Text(list[index].description),
-                            trailing: Row(
-                              mainAxisSize: MainAxisSize.min,
-                              children: [
-                                Text(
-                                  '${list[index].date.day}/${list[index].date.month}',
-                                  style: Theme.of(context)
-                                      .textTheme
-                                      .bodySmall
-                                      ?.copyWith(
-                                          color: Theme.of(context)
-                                              .secondaryHeaderColor),
-                                ),
-                                Icon(
-                                  Icons.check_circle_rounded,
-                                  color: Theme.of(context).primaryColor,
-                                  size: Sizes.smallIconSize,
-                                )
-                              ],
-                            ),
-                          ),
+                          child: TransactionTile(transaction: list[index]),
                         ),
                       ),
                     ),
@@ -105,3 +75,4 @@ class _StatementPageState extends State<StatementPage> {
     );
   }
 }
+
---
+++ lib/features/statement/widgets/transaction_tile.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+import '../../../design_sys/colors.dart';
+import '../../../design_sys/sizes.dart';
+import '../../home/models/transaction.dart';
+
+class TransactionTile extends StatelessWidget {
+  const TransactionTile({
+    Key? key,
+    required this.transaction,
+  }) : super(key: key);
+
+  final Transaction transaction;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        transaction.valueString,
+        style: TextStyle(
+          color: transaction.type == 'expense'
+              ? AppColors.expense
+              : AppColors.income,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+      subtitle: Text(transaction.description),
+      trailing: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text(
+            '${transaction.date.day}/${transaction.date.month}',
+            style: Theme.of(context)
+                .textTheme
+                .bodySmall
+                ?.copyWith(
+                    color: Theme.of(context)
+                        .secondaryHeaderColor),
+          ),
+          Icon(
+            Icons.check_circle_rounded,
+            color: Theme.of(context).primaryColor,
+            size: Sizes.smallIconSize,
+          )
+        ],
+      ),
+    );
+  }
+}
