94f9deb88e7d1b3cede9d5d7a295357496712e65
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-11-26 16:11:12 -0300

    fix: style of tiles

--- lib/features/statement/statement_page.dart
+++ lib/features/statement/statement_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../design_sys/colors.dart';
+import '../../design_sys/sizes.dart';
 import '../home/components/month_changer.dart';
 import 'statement_controller.dart';
 import 'statement_states.dart';
@@ -53,14 +55,46 @@ class _StatementPageState extends State<StatementPage> {
                     ],
                   ),
                   Expanded(
-                    child: ListView.builder(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: Sizes.smallSpace),
+                      child: ListView.builder(
                         itemCount: list.length,
                         itemBuilder: (context, index) => Card(
-                              child: ListTile(
-                                title: Text(list[index].valueString),
-                                subtitle: Text(list[index].description),
+                          child: ListTile(
+                            title: Text(
+                              list[index].valueString,
+                              style: TextStyle(
+                                color: list[index].type == 'expense'
+                                    ? AppColors.expense
+                                    : AppColors.income,
+                                fontWeight: FontWeight.bold,
                               ),
-                            )),
+                            ),
+                            subtitle: Text(list[index].description),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                Text(
+                                  '${list[index].date.day}/${list[index].date.month}',
+                                  style: Theme.of(context)
+                                      .textTheme
+                                      .bodySmall
+                                      ?.copyWith(
+                                          color: Theme.of(context)
+                                              .secondaryHeaderColor),
+                                ),
+                                Icon(
+                                  Icons.check_circle_rounded,
+                                  color: Theme.of(context).primaryColor,
+                                  size: Sizes.smallIconSize,
+                                )
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
                   ),
                 ],
               );
