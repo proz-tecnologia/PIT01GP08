aa4ac349214fb8dc50574860e7bde731ab2ae921
rudarabello <ruda.rabello.silva@gmail.com>
2022-11-29 13:00:32 -0300

    feat: add sizes

--- lib/design_sys/sizes.dart
+++ lib/design_sys/sizes.dart
@@ -4,9 +4,13 @@ class Sizes {
   static const smallSpace = 8.0;
   static const mediumSpace = 16.0;
   static const largeSpace = 24.0;
+  static const extraLargeSpace = 48.0;
+
+  static const logoSize = 100.0;
 
   static const mediumIconSize = 32.0;
   static const largeIconSize = 40.0;
 
   static const threePercent = 0.03;
+  static const dialogFactor = 0.3;
 }
