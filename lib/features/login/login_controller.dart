49c19ec89c11bda8037f3ca92e804f612d61650d
rudarabello <ruda.rabello.silva@gmail.com>
2022-11-28 21:49:35 -0300

    feat: add login controller

---
+++ lib/features/login/login_controller.dart
@@ -0,0 +1,16 @@
+import 'package:financial_app/shared/shared_preferences_keys.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import 'login_states.dart';
+
+class LoginController {
+  Future<LoginState> login(String email, String password) async {
+    final prefs = await SharedPreferences.getInstance();
+
+    if (email == 'ruda@gmail.com' && password == '12345678') {
+      prefs.setBool(SharedPreferencesKeys.userLogged, true);
+      return LoginStateSuccess();
+    }
+    return LoginStateError('senha invalida');
+  }
+}
