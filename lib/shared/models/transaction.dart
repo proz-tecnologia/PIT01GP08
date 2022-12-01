cab0849a6775520e84c252c184e2ededde044568
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-12-01 19:48:41 -0300

    feat: transaction model and adaptations

--- lib/shared/models/transaction.dart
+++ lib/shared/models/transaction.dart
@@ -1,25 +1,84 @@
 enum Type { expense, income }
 
+enum Payment { normal, parcelled, fixed }
+
 class Transaction {
-  final DateTime _date;
-  final String _description;
-  final double _value;
-  final Type _type;
+  final DateTime date;
+  final String description;
+  final double value;
+  final Type type;
+  final int categoryId;
+  final bool fulfilled;
+  final int id;
+  final Payment payment;
 
   Transaction({
-    required DateTime date,
-    required String description,
-    required double value,
-    required Type type,
-  })  : _date = date,
-        _description = description,
-        _value = value,
-        _type = type;
+    required this.date,
+    required this.description,
+    required this.value,
+    required this.type,
+    required this.categoryId,
+    required this.fulfilled,
+    required this.id,
+    required this.payment,
+  });
 
-  DateTime get date => _date;
-  String get description => _description;
-  double get value => _value;
   String get valueString =>
-      'R\$ ${_value.toStringAsFixed(2).replaceFirst('.', ',')}';
-  String get type => _type.name;
+      'R\$ ${value.toStringAsFixed(2).replaceFirst('.', ',')}';
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'date': date.millisecondsSinceEpoch});
+    result.addAll({'description': description});
+    result.addAll({'value': value});
+    result.addAll({'type': type.name});
+    result.addAll({'categoryId': categoryId});
+    result.addAll({'fulfilled': fulfilled});
+    result.addAll({'id': id});
+    result.addAll({'payment': payment.name});
+
+    return result;
+  }
+
+  factory Transaction.fromMap(Map<String, dynamic> map) {
+    final type_ = map['type'] == 'expense' ? Type.expense : Type.income;
+    final payment_ = map['payment'] == 'normal'
+        ? Payment.normal
+        : map['payment'] == 'fixed'
+            ? Payment.fixed
+            : Payment.parcelled;
+    return Transaction(
+      date: DateTime.fromMillisecondsSinceEpoch(map['date']),
+      description: map['description'] ?? '',
+      value: map['value']?.toDouble() ?? 0.0,
+      type: type_,
+      categoryId: map['categoryId']?.toInt() ?? 0,
+      fulfilled: map['fulfilled'] ?? false,
+      id: map['id']?.toInt() ?? 0,
+      payment: payment_,
+    );
+  }
+
+  Transaction copyWith({
+    DateTime? date,
+    String? description,
+    double? value,
+    Type? type,
+    int? categoryId,
+    bool? fulfilled,
+    int? id,
+    Payment? payment,
+  }) {
+    return Transaction(
+      date: date ?? this.date,
+      description: description ?? this.description,
+      value: value ?? this.value,
+      type: type ?? this.type,
+      categoryId: categoryId ?? this.categoryId,
+      fulfilled: fulfilled ?? this.fulfilled,
+      id: id ?? this.id,
+      payment: payment ?? this.payment,
+    );
+  }
 }
