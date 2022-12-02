45ad9438bc135029980c7cd16ff0f60e2877877c
rudarabello <ruda.rabello.silva@gmail.com>
2022-12-02 14:33:36 -0300

    feat: implement states on resumeInfoWidget

--- lib/features/home/widgets/resume_info_widget.dart
+++ lib/features/home/widgets/resume_info_widget.dart
@@ -1,6 +1,9 @@
 import 'package:financial_app/design_sys/sizes.dart';
+import 'package:financial_app/features/home/home_controller.dart';
+import 'package:financial_app/features/home/home_states.dart';
 import 'package:financial_app/shared/widgets/month_changer.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'total_tile.dart';
 
@@ -25,62 +28,71 @@ class _ResumeInfoWidgetState extends State<ResumeInfoWidget> {
       width: MediaQuery.of(context).size.width,
       color: Theme.of(context).primaryColor,
       child: SafeArea(
-        child: ValueListenableBuilder(
-          valueListenable: _isVisible,
-          builder: (context, value, _) {
-            return Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                SizedBox(height: spaceBetween),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: const [
-                    MonthChanger('Dezembro'),
-                  ],
-                ),
-                SizedBox(height: spaceBetween),
-                Row(
+        child: BlocBuilder<HomeController, HomeState>(
+          builder: (context, state) {
+            final controller = context.read<HomeController>();
+            return ValueListenableBuilder(
+              valueListenable: _isVisible,
+              builder: (context, value, _) {
+                return Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
                   children: [
-                    Expanded(flex: 1, child: Container()),
-                    TotalTile(
-                      label: 'Saldo',
-                      value: 'R\$ 2600,00',
-                      visible: _isVisible.value,
+                    SizedBox(height: spaceBetween),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        MonthChanger('Dezembro'),
+                      ],
                     ),
-                    Expanded(
-                      flex: 1,
-                      child: IconButton(
-                        onPressed: () => _isVisible.value = !_isVisible.value,
-                        icon: Icon(
-                          _isVisible.value
-                              ? Icons.visibility
-                              : Icons.visibility_off,
-                          color: Theme.of(context).colorScheme.onPrimary,
+                    SizedBox(height: spaceBetween),
+                    Row(
+                      children: [
+                        Expanded(flex: 1, child: Container()),
+                        TotalTile(
+                          label: 'Saldo',
+                          value:
+                              'R\$ ${controller.displayBalance('balance').toStringAsFixed(2).replaceAll('.', ',')}',
+                          visible: _isVisible.value,
                         ),
-                      ),
+                        Expanded(
+                          flex: 1,
+                          child: IconButton(
+                            onPressed: () =>
+                                _isVisible.value = !_isVisible.value,
+                            icon: Icon(
+                              _isVisible.value
+                                  ? Icons.visibility
+                                  : Icons.visibility_off,
+                              color: Theme.of(context).colorScheme.onPrimary,
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
+                    SizedBox(height: spaceBetween),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      children: [
+                        TotalTile(
+                          icon: Icons.arrow_downward,
+                          label: 'Despesas',
+                          value:
+                              'R\$ ${controller.displayBalance('expense').toStringAsFixed(2).replaceAll('.', ',')}',
+                          visible: _isVisible.value,
+                        ),
+                        TotalTile(
+                          icon: Icons.arrow_upward,
+                          label: 'Receitas',
+                          value:
+                              'R\$ ${controller.displayBalance('income').toStringAsFixed(2).replaceAll('.', ',')}',
+                          visible: _isVisible.value,
+                        ),
+                      ],
+                    ),
+                    SizedBox(height: spaceBetween),
                   ],
-                ),
-                SizedBox(height: spaceBetween),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    TotalTile(
-                      icon: Icons.arrow_downward,
-                      label: 'Despesas',
-                      value: 'R\$ 150,00',
-                      visible: _isVisible.value,
-                    ),
-                    TotalTile(
-                      icon: Icons.arrow_upward,
-                      label: 'Receitas',
-                      value: 'R\$ 1000,00',
-                      visible: _isVisible.value,
-                    ),
-                  ],
-                ),
-                SizedBox(height: spaceBetween),
-              ],
+                );
+              },
             );
           },
         ),
