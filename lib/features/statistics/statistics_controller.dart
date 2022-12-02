c4da0ea66c1b56203bc664001d1552660ef2bd91
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-12-02 17:54:01 -0300

    feat: integrate with repository

--- lib/features/statistics/statistics_controller.dart
+++ lib/features/statistics/statistics_controller.dart
@@ -1,5 +1,8 @@
 import 'dart:math';
 
+import 'package:financial_app/shared/category_repository.dart';
+import 'package:financial_app/shared/models/category.dart';
+import 'package:financial_app/shared/transaction_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
@@ -24,23 +27,43 @@ class StatisticsController extends Cubit<StatisticsState> {
       // - pegar despesas do mês
       // - separar em categorias
       // - gerar seções com base nos valores totais
-      _sections.addAll([
-        Section(5, description: 'Section 1', color: Colors.blue),
-        Section(4, description: 'Section 2', color: Colors.amber),
-        Section(3, description: 'Section 3', color: Colors.purple),
-        Section(2, description: 'Section 4', color: Colors.green),
-        Section(0.5, description: 'Section 5', color: Colors.pink),
-      ]);
+      // _sections.addAll([
+      //   Section(5, description: 'Section 1', color: Colors.blue),
+      //   Section(4, description: 'Section 2', color: Colors.amber),
+      //   Section(3, description: 'Section 3', color: Colors.purple),
+      //   Section(2, description: 'Section 4', color: Colors.green),
+      //   Section(0.5, description: 'Section 5', color: Colors.pink),
+      // ]);
+      final categories = await CategoryDioRepository().getAllCategories();
+      final transactions =
+          await TransactionDioRepository().getAllTransactions();
+      Map<int, double> map = {};
+
+      for (var transaction in transactions) {
+        if (map.containsKey(transaction.categoryId)) {
+          map[transaction.categoryId] =
+              map[transaction.categoryId]! + transaction.value;
+        }
+        map.addAll({transaction.categoryId: transaction.value});
+      }
+      for (var entry in map.entries) {
+        final category =
+            categories.firstWhere((element) => element.id == entry.key);
+        _sections.add(
+          Section(entry.value,
+              description: category.name, color: category.color),
+        );
+      }
 
       _total = _getTotal();
       _setPercents();
 
       // para testes de gerência de estado:
-      await Future.delayed(const Duration(seconds: 1));
-      final random = Random();
-      if (random.nextBool() && random.nextBool()) {
-        throw Exception();
-      }
+      // await Future.delayed(const Duration(seconds: 1));
+      // final random = Random();
+      // if (random.nextBool() && random.nextBool()) {
+      //   throw Exception();
+      // }
 
       emit(SuccessStatisticsState());
     } catch (e) {
