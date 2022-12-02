dc42a29a1c9e67edb8e4697958fe69b7be72301c
rudarabello <ruda.rabello.silva@gmail.com>
2022-12-02 20:05:21 -0300

    feat: blockBuilder on app_pending

--- lib/features/home/widgets/app_pending.dart
+++ lib/features/home/widgets/app_pending.dart
@@ -1,7 +1,11 @@
 import 'package:financial_app/design_sys/colors.dart';
 import 'package:financial_app/design_sys/sizes.dart';
+import 'package:financial_app/features/home/home_controller.dart';
+import 'package:financial_app/features/home/home_states.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../statement/statement_states.dart';
 import 'pending_card.dart';
 
 class AppPending extends StatelessWidget {
@@ -11,30 +15,43 @@ class AppPending extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(Sizes.smallSpace),
-      child: Row(
-        children: const [
-          Expanded(
-            flex: 1,
-            child: PendingCard(
-              icon: Icons.download_rounded,
-              color: AppColors.expense,
-              label: 'A pagar',
-              value: 'R\$ 1000,00',
+    return BlocBuilder<HomeController, HomeState>(
+      builder: (context, state) {
+        final controller = context.read<HomeController>();
+        if (state is ErrorStatementState) {
+          return const Center(child: Text('Erro ao carregar os dados'));
+        }
+        if (state is SuccessHomeState) {
+          return Padding(
+            padding: const EdgeInsets.all(Sizes.smallSpace),
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: PendingCard(
+                    icon: Icons.download_rounded,
+                    color: AppColors.expense,
+                    label: 'A pagar',
+                    value:
+                        'R\$ ${controller.displayBalance('pendingExpense').toStringAsFixed(2).replaceAll('.', ',')}',
+                  ),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: PendingCard(
+                    color: AppColors.income,
+                    icon: Icons.publish_rounded,
+                    label: 'A receber',
+                    value:
+                        'R\$ ${controller.displayBalance('pendingIncome').toStringAsFixed(2).replaceAll('.', ',')}',
+                  ),
+                ),
+              ],
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: PendingCard(
-              color: AppColors.income,
-              icon: Icons.publish_rounded,
-              label: 'A receber',
-              value: 'R\$ 1000,00',
-            ),
-          ),
-        ],
-      ),
+          );
+        }
+        return const Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
