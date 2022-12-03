3f4b8f244f098e11938855d79d41dda29b22b94e
Renata Sisilio <112419444+RenataSisilio@users.noreply.github.com>
2022-12-02 23:37:46 -0300

    Update new_entry_page.dart

--- lib/features/new_entry/new_entry_page.dart
+++ lib/features/new_entry/new_entry_page.dart
@@ -1,48 +1,48 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
 import 'new_entry_content.dart';
 import 'new_entry_controller.dart';
 import 'new_entry_states.dart';
-
 class NewEntryPage extends StatelessWidget {
   const NewEntryPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Nova transação')),
-      body: BlocConsumer<NewEntryController, NewEntryState>(
-        listener: (context, currentState) {
-          if (currentState is SavingErrorNewEntryState) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Ocorreu um erro. Tente novamente.'),
-              ),
-            );
-          }
-          if (currentState is SuccessNewEntryState) {
-            ScaffoldMessenger.of(context).showSnackBar(
-              const SnackBar(
-                content: Text('Transação salva com sucesso.'),
-              ),
-            );
-            Navigator.of(context).pop();
-          }
-        },
-        builder: (context, currentState) {
-          if (currentState is ErrorNewEntryState) {
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(title: const Text('Nova transação')),
+        body: BlocConsumer<NewEntryController, NewEntryState>(
+          listener: (context, currentState) {
+            if (currentState is SavingErrorNewEntryState) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Ocorreu um erro. Tente novamente.'),
+                ),
+              );
+            }
+            if (currentState is SuccessNewEntryState) {
+              ScaffoldMessenger.of(context).showSnackBar(
+                const SnackBar(
+                  content: Text('Transação salva com sucesso.'),
+                ),
+              );
+              Navigator.of(context).pop();
+            }
+          },
+          builder: (context, currentState) {
+            if (currentState is ErrorNewEntryState) {
+              return const Center(
+                child: Text('Erro'),
+              );
+            }
+            if (currentState is NewEntryTypeState) {
+              return const NewEntryContent();
+            }
             return const Center(
-              child: Text('Erro'),
+              child: CircularProgressIndicator(),
             );
-          }
-          if (currentState is NewEntryTypeState) {
-            return const NewEntryContent();
-          }
-          return const Center(
-            child: CircularProgressIndicator(),
-          );
-        },
+          },
+        ),
       ),
     );
   }
