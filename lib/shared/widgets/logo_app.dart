ecf68b1a6772fd253c8033084b711e81e41f8c94
rudarabello <ruda.rabello.silva@gmail.com>
2022-12-02 21:18:54 -0300

    refactor: logo folder

---
+++ lib/shared/widgets/logo_app.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../../design_sys/sizes.dart';
+
+class LogoApp extends StatelessWidget {
+  const LogoApp({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Image(
+      width: Sizes.logoSize,
+      height: Sizes.logoSize,
+      image: AssetImage('assets/logo_colors.png'),
+    );
+  }
+}
