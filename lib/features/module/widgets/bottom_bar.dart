42bcc95ea489a8c91fd0c1de284306be061a02e9
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-12-13 13:23:26 -0300

    refactor: convert to valuenotifier management

--- lib/features/module/widgets/bottom_bar.dart
+++ lib/features/module/widgets/bottom_bar.dart
@@ -1,59 +1,58 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../design_sys/sizes.dart';
 import '../data_controller.dart';
 import '../data_states.dart';
 
-class BottomBar extends StatefulWidget {
+class BottomBar extends StatelessWidget {
   const BottomBar({
-    Key? key,
+    super.key,
     required this.controller,
-  }) : super(key: key);
+  });
 
   final PageController controller;
 
   @override
-  State<BottomBar> createState() => _BottomBarState();
-}
-
-class _BottomBarState extends State<BottomBar> {
-  int index = 0;
-
-  @override
   Widget build(BuildContext context) {
+    final index = ValueNotifier(0);
+
     return BlocBuilder<DataController, DataState>(
-      builder: (context, state) => BottomNavigationBar(
-        items: [
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.home_rounded),
-            label: 'Início',
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.import_export_rounded),
-            label: 'Extrato',
-          ),
-          BottomNavigationBarItem(
-              icon: Container(),
-              label:
-                  ''), // item vazio com o único objetivo de melhorar o layout
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.equalizer_rounded),
-            label: 'Estatística',
-          ),
-          const BottomNavigationBarItem(
-            icon: Icon(Icons.more_horiz),
-            label: 'Mais',
-          ),
-        ],
-        currentIndex: index,
-        onTap: (index) {
-          if (state is! LoadingDataState) {
-            setState(() {
-              widget.controller.jumpToPage(index < 2 ? index : index - 1);
-              this.index = index;
-            });
-          }
-        },
+      builder: (context, state) => ValueListenableBuilder(
+        valueListenable: index,
+        builder: (_, value, __) => BottomNavigationBar(
+          items: const [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home_rounded),
+              label: 'Início',
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: EdgeInsets.only(right: Sizes.extraLargeSpace),
+                child: Icon(Icons.import_export_rounded),
+              ),
+              label: 'Extrato',
+            ),
+            BottomNavigationBarItem(
+              icon: Padding(
+                padding: EdgeInsets.only(left: Sizes.extraLargeSpace),
+                child: Icon(Icons.equalizer_rounded),
+              ),
+              label: 'Estatística',
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.more_horiz),
+              label: 'Mais',
+            ),
+          ],
+          currentIndex: value,
+          onTap: (newIndex) {
+            if (state is! LoadingDataState) {
+              controller.jumpToPage(newIndex);
+              index.value = newIndex;
+            }
+          },
+        ),
       ),
     );
   }
