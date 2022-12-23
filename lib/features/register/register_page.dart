03aa886af2867b9f91a44a3675479884f8d1b483
rudarabello <ruda.rabello.silva@gmail.com>
2022-12-23 15:08:36 -0300

    refactor: change repositories folder

--- lib/features/register/register_page.dart
+++ lib/features/register/register_page.dart
@@ -2,7 +2,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-import '../../shared/category_repository.dart';
+import '../../shared/repositories/category_repository.dart';
 import '../../shared/widgets/logo_app.dart';
 import 'models/user.dart';
 import 'register_controller.dart';
