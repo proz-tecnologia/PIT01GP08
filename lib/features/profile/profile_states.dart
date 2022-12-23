36e1229b5bdcd54d281bc3fc3214ed2bb7db899e
rudarabello <ruda.rabello.silva@gmail.com>
2022-12-23 15:07:56 -0300

    fix: update states on profile

--- lib/features/profile/profile_states.dart
+++ lib/features/profile/profile_states.dart
@@ -1,8 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
 abstract class ProfileState {}
 
 class LoadingProfileState implements ProfileState {}
 
-class SuccessProfileState implements ProfileState {}
+class SuccessProfileState implements ProfileState {
+  final User user;
+
+  SuccessProfileState(this.user);
+}
 
 class ErrorProfileState implements ProfileState {
   final String error;
