6c91884c204aa64419d8c369022c3900770ac09d
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2022-12-27 21:15:04 -0300

    feat: month closing label

--- lib/features/page_view/home/home_content_page.dart
+++ lib/features/page_view/home/home_content_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import '../../../design_sys/sizes.dart';
+import '../../../shared/utils/end_of_month.dart';
 import '../data_controller.dart';
 import '../data_states.dart';
 import 'home_controller.dart';
@@ -21,8 +22,19 @@ class HomeContentPage extends StatelessWidget {
       child: BlocProvider(
         create: (context) => HomeController(transactionList),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const ResumeInfoWidget(),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(horizontal: Sizes.mediumSpace),
+              child: Text(
+                'Até o dia ${DateTime.now().endOfMonthStr()}',
+                style: Theme.of(context).textTheme.bodyLarge?.copyWith(
+                      color: Theme.of(context).disabledColor,
+                    ),
+              ),
+            ),
             Padding(
               padding: const EdgeInsets.all(Sizes.smallSpace),
               child: Row(
