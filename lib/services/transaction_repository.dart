789ca4e41da44fddc5cb52ddf6ddcf9194fe5593
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2023-01-05 16:35:02 -0300

    refactor: filter saving to db

--- lib/services/transaction_repository.dart
+++ lib/services/transaction_repository.dart
@@ -2,6 +2,7 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 import '../shared/models/transaction.dart' as model;
+import '../shared/models/transaction.dart';
 
 abstract class TransactionRepository {
   Future<bool> createTransaction(model.Transaction transaction);
@@ -14,13 +15,18 @@ abstract class TransactionRepository {
 class TransactionFirebaseRepository implements TransactionRepository {
   final firestorePath = FirebaseFirestore.instance
       .collection('users')
-      .doc(FirebaseAuth.instance.currentUser!.uid)
-      .collection('transactions');
+      .doc(FirebaseAuth.instance.currentUser!.uid);
 
   @override
   Future<bool> createTransaction(model.Transaction transaction) async {
     try {
-      firestorePath.add(transaction.toMap());
+      if (transaction.payment == Payment.fixa) {
+        firestorePath.collection('fixed-transactions').add(transaction.toMap());
+      } else if (transaction.payment == Payment.parcelada) {
+        firestorePath.collection('parcelled-transactions').add(transaction.toMap());
+      } else {
+        firestorePath.collection('transactions').add(transaction.toMap());
+      }
       return true;
     } catch (e) {
       return false;
@@ -30,7 +36,7 @@ class TransactionFirebaseRepository implements TransactionRepository {
   @override
   Future<bool> deleteTransaction(String id) async {
     try {
-      firestorePath.doc(id).delete();
+      firestorePath.collection('transactions').doc(id).delete();
       return true;
     } catch (e) {
       return false;
@@ -40,7 +46,10 @@ class TransactionFirebaseRepository implements TransactionRepository {
   @override
   Future<bool> editTransactionData(model.Transaction transaction) async {
     try {
-      firestorePath.doc(transaction.id).set(transaction.toMap());
+      firestorePath
+          .collection('transactions')
+          .doc(transaction.id)
+          .set(transaction.toMap());
       return true;
     } catch (e) {
       return false;
@@ -51,8 +60,10 @@ class TransactionFirebaseRepository implements TransactionRepository {
   Future<List<model.Transaction>> getAllTransactions() async {
     final list = <model.Transaction>[];
     try {
-      final snapshot =
-          await firestorePath.orderBy("date", descending: true).get();
+      final snapshot = await firestorePath
+          .collection('transactions')
+          .orderBy("date", descending: true)
+          .get();
       final docs = snapshot.docs;
       for (var doc in docs) {
         list.add(
@@ -68,7 +79,8 @@ class TransactionFirebaseRepository implements TransactionRepository {
   @override
   Future<model.Transaction?> getTransactionData(String id) async {
     try {
-      final snapshot = await firestorePath.doc(id).get();
+      final snapshot =
+          await firestorePath.collection('transactions').doc(id).get();
       final data = snapshot.data()!;
       return model.Transaction.fromMap(id, data);
     } catch (e) {
