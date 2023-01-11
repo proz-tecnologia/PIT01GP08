3a9ca5ed1f73a6f8da1c47e0104e6e5fd7564e93
jacksonlira88 <jacksonlira.rs@gmail.com>
2023-01-10 22:06:47 -0300

    feat: page of the settings

---
+++ lib/features/page_view/settings/settings_page.dart
@@ -0,0 +1,24 @@
+//import 'package:financial_app/features/settings/settings_controller.dart';
+import 'package:flutter/material.dart';
+
+//import 'widgets/select_conf_bio.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        children: const [
+          ListTile(
+            title: Text("Segurança"),
+          ),
+
+          //SelecConfBio(titleSelected: 'Login com email'),
+        ],
+      ),
+    );
+  }
+}
