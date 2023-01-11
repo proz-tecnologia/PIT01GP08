3bef4df97f00202892ff8f8ac2b3d90cda612074
rudarabello <ruda.rabello.silva@gmail.com>
2023-01-11 17:11:11 -0300

    feat: google sign_in on login controller

--- lib/features/login/login_controller.dart
+++ lib/features/login/login_controller.dart
@@ -2,6 +2,7 @@ import 'dart:developer';
 
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 import 'login_states.dart';
 
 class LoginController extends Cubit<LoginState> {
@@ -17,6 +18,28 @@ class LoginController extends Cubit<LoginState> {
       if (e is FirebaseAuthException) {
         log(e.message ?? 'FirebaseAuthException');
         emit(LoginStateError(e.message ?? 'Error on loginController'));
+      }
+    }
+  }
+
+  Future<void> googleSignIn() async {
+    final googleSignIn = GoogleSignIn();
+    final googleAccount = await googleSignIn.signIn();
+    if (googleAccount != null) {
+      final googleAuth = await googleAccount.authentication;
+      if (googleAuth.accessToken != null && googleAuth.idToken != null) {
+        try {
+          await FirebaseAuth.instance.signInWithCredential(
+            GoogleAuthProvider.credential(
+                idToken: googleAuth.idToken,
+                accessToken: googleAuth.accessToken),
+          );
+          emit(LoginStateSuccess());
+        } catch (e) {
+          if (e is FirebaseAuthException) {
+            emit(LoginStateError(e.message ?? 'Error on loginController'));
+          }
+        }
       }
     }
   }
