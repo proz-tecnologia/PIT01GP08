58b187a881698faf4369de01a6b86922de20735c
jacksonlira88 <jacksonlira.rs@gmail.com>
2023-01-10 23:41:42 -0300

    feat:  add biometric controller.

--- lib/features/splash/splash_controller.dart
+++ lib/features/splash/splash_controller.dart
@@ -1,24 +1,57 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:local_auth_android/local_auth_android.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../firebase_options.dart';
 
-enum SplashState { loading, logged, unlogged }
+enum SplashState { loading, logged, unlogged, manualLog }
 
 class SplashController extends Cubit<SplashState> {
   SplashController() : super(SplashState.loading);
+  final LocalAuthentication authBio = LocalAuthentication();
 
   void init() async {
     await Firebase.initializeApp(
       options: DefaultFirebaseOptions.currentPlatform,
     );
-    //await Future.delayed(const Duration(seconds: 3));
     final user = FirebaseAuth.instance.currentUser;
     if (user == null) {
       emit(SplashState.unlogged);
     } else {
-      emit(SplashState.logged);
+      final SharedPreferences spref = await SharedPreferences.getInstance();
+      final bool canAuthenticateWithBiometrics = await isBiometricAvailable();
+
+      if (canAuthenticateWithBiometrics &&
+          (spref.getBool('biometria') ?? false)) {
+        final a = await authenticate();
+        if (a) {
+          emit(SplashState.logged);
+        } else {
+          emit(SplashState.manualLog);
+        }
+      } else {
+        emit(SplashState.logged);
+      }
     }
+  }
+
+  Future<bool> authenticate() async {
+    return await authBio.authenticate(
+        localizedReason: 'Desbloqueie seu celular',
+        authMessages: const <AuthMessages>[
+          AndroidAuthMessages(
+            biometricHint: '',
+            signInTitle: 'Financial',
+            cancelButton: 'Cancelar',
+          ),
+        ]);
+  }
+
+  Future<bool> isBiometricAvailable() async {
+    final bool canAuthenticateWithBiometrics = await authBio.canCheckBiometrics;
+    return canAuthenticateWithBiometrics && await authBio.isDeviceSupported();
   }
 }
