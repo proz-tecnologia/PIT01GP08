03c72f488354f8b15f96b32912eafb75e9d36009
rudarabello <ruda.rabello.silva@gmail.com>
2023-01-17 16:03:21 -0300

    feat: add test to create category

--- test/category_repository_test.dart
+++ test/category_repository_test.dart
@@ -1,5 +1,7 @@
 import 'package:fake_cloud_firestore/fake_cloud_firestore.dart';
+import 'package:financial_app/shared/models/category.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
@@ -12,7 +14,7 @@ void main() {
   late CategoryFirebaseRepository categoryFirebaseRepository;
   late FakeFirebaseFirestore firestoreMock;
 
-  setUp(() async {
+  setUp(() {
     uid = '';
     firestoreMock = FakeFirebaseFirestore();
     categoryFirebaseRepository = CategoryFirebaseRepository(
@@ -22,9 +24,21 @@ void main() {
   });
 
   group("test all category methods", () {
-    test("Should create category", () {
-      const result = null;
-      expect(result, isNull);
+    test("Should create category", () async {
+      final newCategory = Category(
+          color: Colors.black,
+          name: "Categoria",
+          type: Type.income,
+          icon: Icons.abc_outlined);
+      await categoryFirebaseRepository.createCategory(newCategory);
+      final docsCreated = await categoryFirebaseRepository.firestorePath.get();
+      bool result = false;
+      for (var doc in docsCreated.docs) {
+        if (doc.data()["name"] == "Categoria") {
+          result = true;
+        }
+      }
+      expect(result, isTrue);
     });
   });
 }
