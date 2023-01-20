abd1b29b3ce75def8f34a890427cfefb2331aea9
Renata Sisilio <renatamelosisiliosilva@gmail.com>
2023-01-20 14:35:21 -0300

    test: select by date

--- test/shared/utils/select_by_date_test.dart
+++ test/shared/utils/select_by_date_test.dart
@@ -41,6 +41,16 @@ void main() {
       expect(result.isEmpty, true);
     });
 
+    test('month out of list range 2', () {
+      list.add(exampleTransaction.copyWith(date: DateTime(2023, 3)));
+      list.add(exampleTransaction.copyWith(date: DateTime(2023, 2)));
+      list.sort((a, b) => b.date.millisecondsSinceEpoch
+          .compareTo(a.date.millisecondsSinceEpoch));
+
+      final result = list.getMonthRange(january);
+      expect(result.isEmpty, true);
+    });
+
     test('one element selected', () {
       list.add(exampleTransaction.copyWith(date: DateTime(2022)));
       list.add(exampleTransaction.copyWith(date: DateTime(2022, 12)));
@@ -54,4 +64,66 @@ void main() {
       expect(result[0].date, DateTime(2023, 1));
     });
   });
+
+  group('getUntilDate()', () {
+    test('empty list', () {
+      final result = list.getUntilDate(january);
+      expect(result.isEmpty, true);
+    });
+
+    test('no transaction in period', () {
+      list.add(exampleTransaction.copyWith(date: DateTime(2024)));
+      list.add(exampleTransaction.copyWith(date: DateTime(2023, 2)));
+      list.sort((a, b) => b.date.millisecondsSinceEpoch
+          .compareTo(a.date.millisecondsSinceEpoch));
+
+      final result = list.getUntilDate(january);
+      expect(result.isEmpty, true);
+    });
+
+    test('success case', () {
+      list.add(exampleTransaction.copyWith(date: DateTime(2022)));
+      list.add(exampleTransaction.copyWith(date: DateTime(2022, 12)));
+      list.add(exampleTransaction.copyWith(date: DateTime(2023, 2)));
+      list.add(exampleTransaction.copyWith(date: DateTime(2023, 1)));
+      list.add(exampleTransaction.copyWith(
+          date: january.add(const Duration(days: 1))));
+      list.sort((a, b) => b.date.millisecondsSinceEpoch
+          .compareTo(a.date.millisecondsSinceEpoch));
+
+      final result = list.getUntilDate(january);
+      expect(result.length, 3);
+    });
+  });
+
+group('getUntilMonth()', () {
+    test('empty list', () {
+      final result = list.getUntilMonth(january);
+      expect(result.isEmpty, true);
+    });
+
+    test('no transaction in period', () {
+      list.add(exampleTransaction.copyWith(date: DateTime(2024)));
+      list.add(exampleTransaction.copyWith(date: DateTime(2023, 2)));
+      list.sort((a, b) => b.date.millisecondsSinceEpoch
+          .compareTo(a.date.millisecondsSinceEpoch));
+
+      final result = list.getUntilMonth(january);
+      expect(result.isEmpty, true);
+    });
+
+    test('success case', () {
+      list.add(exampleTransaction.copyWith(date: DateTime(2022)));
+      list.add(exampleTransaction.copyWith(date: DateTime(2022, 12)));
+      list.add(exampleTransaction.copyWith(date: DateTime(2023, 2)));
+      list.add(exampleTransaction.copyWith(date: DateTime(2023, 1)));
+      list.add(exampleTransaction.copyWith(
+          date: january.add(const Duration(days: 1))));
+      list.sort((a, b) => b.date.millisecondsSinceEpoch
+          .compareTo(a.date.millisecondsSinceEpoch));
+
+      final result = list.getUntilMonth(january);
+      expect(result.length, 4);
+    });
+  });  
 }
